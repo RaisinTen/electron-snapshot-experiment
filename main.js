e16647e4eb7386c24eaacfae4e31b58321fa1bed
Darshan Sen <raisinten@gmail.com>
2024-08-09 18:28:45 +0530

    Add ejs/async and rimraf to the snapshot
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- main.js
+++ main.js
@@ -21,6 +21,26 @@ const circularJSON = require('circular-json');
 const _ = require('lodash');
 const async = require('async');
 const i18n = require('i18next');
+const jsonStorage = require('electron-json-storage');
+
+// Make require('glob').Glob inherit from EventEmitter. This needs to be done
+// here because EventEmitter is not a part of the default V8 context which is
+// used to generate the V8 snapshot.
+{
+  const glob = require('glob');
+  const util = require('node:util');
+  const EE = require('node:events').EventEmitter;
+  util.inherits(glob.Glob, EE);
+}
+
+// Set minimatch.sep with path.sep. This needs to be done here because the
+// 'node:path' module is not a part of the default V8 context which is
+// used to generate the V8 snapshot.
+{
+  const minimatch = require('minimatch');
+  const path = require('node:path');
+  minimatch.sep = path.sep
+}
 
 const { performance } = require('node:perf_hooks');
 performance.measure('startup');
--- snapshot.js
+++ snapshot.js
@@ -3,3 +3,7 @@ require('circular-json');
 require('lodash');
 require('async');
 require('i18next');
+
+// Snapshottable dependencies of 'electron-json-storage'.
+require('electron-json-storage/node_modules/async');
+require('rimraf');
