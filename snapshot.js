997aeba1fd6dd55603c328fab48d37638c31bbb7
Darshan Sen <raisinten@gmail.com>
2024-08-16 16:03:47 +0530

    Rearrange snapshot.js
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- snapshot.js
+++ snapshot.js
@@ -1,24 +1,35 @@
+// Snapshottable dependencies
+
 require('semver');
 require('circular-json');
 require('lodash');
 require('async');
 require('i18next');
 
-// Snapshottable dependencies of 'electron-json-storage'.
-require('electron-json-storage/node_modules/async');
-require('rimraf');
-require('mkdirp');
+// Snapshottable dependencies of 'electron-json-storage'
+{
+  require('electron-json-storage/node_modules/async');
+  require('rimraf');
+  require('mkdirp');
 
-// Snapshottable dependencies of 'write-file-atomic'.
-  // Snapshottable dependencies of 'graceful-fs'.
-  // TODO(RaisinTen): Why does require('graceful-fs/polyfills'); not work?
-  require('graceful-fs/legacy-streams');
-  require('graceful-fs/clone');
-require('imurmurhash');
-// TODO(RaisinTen): Can we add require('signal-exit');?
-require('signal-exit/signals');
+  // Snapshottable dependencies of 'write-file-atomic'
+  {
+    // Snapshottable dependencies of 'graceful-fs'
+    {
+      // TODO(RaisinTen): Why does require('graceful-fs/polyfills'); not work?
+      require('graceful-fs/legacy-streams');
+      require('graceful-fs/clone');
+    }
+    require('imurmurhash');
+    // Snapshottable dependencies of 'signal-exit'
+    {
+      // TODO(RaisinTen): Can we add require('signal-exit');?
+      require('signal-exit/signals');
+    }
+  }
 
-require('electron-json-storage/lib/utils');
-require('electron-json-storage/lib/lock');
+  require('electron-json-storage/lib/utils');
+  require('electron-json-storage/lib/lock');
+}
 
 require('uuid/v4');
